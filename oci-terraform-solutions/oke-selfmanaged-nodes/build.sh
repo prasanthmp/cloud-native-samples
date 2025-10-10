2071b83f442d94b933272136022ead54828822cc
Prasanth Prasad <prasanth.prasad@oracle.com>
2025-10-10 15:49:20 -0400

    Updated output message.

--- oci-terraform-solutions/oke-selfmanaged-nodes/build.sh
+++ oci-terraform-solutions/oke-selfmanaged-nodes/build.sh
@@ -9,7 +9,10 @@ terraform apply -var-file="terraform.local.tfvars" -parallelism=5 -auto-approve
 END=$(date +%s)
 DURATION=$((END - START))
 
-echo "You can find the kubeconfig file at ~/.kube/config"
+echo "Kubeconfig is created and you can find the kubeconfig file at ~/.kube/config."
+echo "You can then use kubectl to interact with your cluster."
+echo "For example, to get the list of nodes, run:"
+echo "kubectl get nodes"
 echo ""
 echo "---------------------------------"
 echo "$(date '+%Y-%m-%d %H:%M:%S') Terraform run took $((DURATION / 60)) minutes and $((DURATION % 60)) seconds" | tee -a terraform_runtime.log
